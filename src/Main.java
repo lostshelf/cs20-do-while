2c43fd289cf0cae0f798387746b54bc999a71d00
Hadi Faraz <hadif2@educbe.ca>
2023-02-06 15:26:46 -0700

    Solve second question with for loop

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     // I have prefixed the solutions with their corresponding assignment and question number
     public static void main(String[] args) {
-        while_two();
+        whileTwo();
+        forTwo();
     }
 
-    static void while_two() {
+    static void whileTwo() {
         int counter = 0;
         do {
             System.out.println(counter);
             counter += 3;
         } while (counter <= 21);
+    }
+
+    static void forTwo() {
+        for (int i = 0; i <= 21; i += 3)
+            System.out.println(i);
     }
 }
