7f6f0e461e8a16d77c374320befc78e1376847ca
Hadi Faraz <hadif2@educbe.ca>
2023-02-06 15:24:50 -0700

    Solve second question

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    // I have prefixed the solutions with their corresponding assignment and question number
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while_two();
+    }
+
+    static void while_two() {
+        int counter = 0;
+        do {
+            System.out.println(counter);
+            counter += 3;
+        } while (counter <= 21);
     }
 }
