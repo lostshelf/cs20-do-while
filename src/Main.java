64a34988131293bdd28601a998e07404a669e942
Hadi Faraz <hadif2@educbe.ca>
2023-02-07 12:37:08 -0700

    Solve question 4 using a while loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class Main {
     // I have prefixed the solutions with their corresponding assignment and question number
     public static void main(String[] args) {
-        whileTwo();
-        forTwo();
+        whileFour();
     }
+
     static void whileTwo() {
         int counter = 0;
         do {
@@ -17,8 +18,18 @@ public class Main {
             System.out.println(i);
     }
 
+    // Ask a user for a number, and alert all the powers of 2 that are less than that number. For example, if 50 is entered, your program should print 1, 2, 4, 8, 16 and 32 to the console.
     static void whileFour() {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int num = Integer.parseInt(scan.nextLine().trim());
+
+        int counter = 1;
+        while (counter < num) {
+            System.out.println(counter);
+            counter *= 2;
+        }
     }
 
     static void forFour() {
